8509da665984aa23bc72793ed8e9de51e22ea9f1
sangmin4208 <sangmin4208@gmail.com>
2022-01-27 21:28:19 +0900

    ✍️폴더 작성, 설정 읽기

---
+++ helpers.go
@@ -0,0 +1,26 @@
+package celeritas
+
+import "os"
+
+func (c *Celeritas) CreateDirIfNotExist(path string) error {
+	const mode = 0755
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, mode)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (c *Celeritas) CreateFileIfNotExist(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			return err
+		}
+		defer func(file *os.File) {
+			_ = file.Close()
+		}(file)
+	}
+	return nil
+}
